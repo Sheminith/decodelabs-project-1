df3a93093777b4b32bfe4e54b910694d2a5578fd
sheminith <10554sheminith@gmail.com>
2026-07-21 16:27:36 +0530

    Implemented a simple chatbot using intent matching using python dictionaries

---
+++ main.py
@@ -0,0 +1,28 @@
+# ====================== KNOWLEDGE BASE ====================== #
+responses = {
+    "hi": "Hello there!",
+    "bye": "Bye... See you again!"
+}
+
+# ======================= MAIN PROGRAM ======================= #
+
+# Greeting messages
+print("========================================")
+print("Welcome to the Chatbot")
+print("========================================")
+print()
+print("Enter 'Exit' to exit")
+print()
+
+# Chat loop
+while True:
+    # Get a cleaned user input
+    user_input = input("You: ").lower().strip() # lower() --> Covert string to lowercase  | strip() --> Remove spaces/tabs/newlines from strings
+
+    # Exit command
+    if user_input == "exit":
+        break
+    
+    # Bot reply
+    reply = responses.get(user_input, "Sorry! I did not understand. Could you rephrase your question please?")
+    print("Bot: ", reply)
